b9ddebc2ca4b7a384c306f25caae006c65a1cf6c
Calvo <calvinn@hotmail.co.uk>
2020-08-26 23:07:57 +0100

    Updated

--- 02_trivia_api/final/backend/test_flaskr.py
+++ 02_trivia_api/final/backend/test_flaskr.py
@@ -15,7 +15,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://calvo@localhost:5432/trivia_test"
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,7 +33,7 @@ class TriviaTestCase(unittest.TestCase):
     [DONE] Write at least one test for each test for successful operation and for expected errors.
     """
 
-    'CC - test created for retrieve_categories.
+    #CC - test created for retrieve_categories.
     def test_retrieve_categories(self):
         res = self.client().get('/categories')
         data = json.loads(res.data)
@@ -41,8 +41,18 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(data['categories'])
+    
+    #CC - test created for retrieve_categories_failure.
+    def test_retrieve_categories_failure(self):
+        res = self.client().get('/categories/120')
+        data = json.loads(res.data)
 
-    'CC - test created for retrieve_questions.
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Not found.')
+
+
+    #CC - test created for retrieve_questions.
     def test_retrieve_questions(self):
         res = self.client().get('/questions')
         data = json.loads(res.data)
@@ -53,36 +63,55 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['total_questions'])
         self.assertTrue(data['categories'])
 
-    'CC - test created for delete_question.
+    #CC - test created for retrieve_questions_failure.
+    def test_retrieve_questions_failure(self):
+        res = self.client().get('/questions/')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Not found.')
+
+    #CC - test created for delete_question.
     def test_delete_question(self):
-        question = Question(question='test question', answer='test answer', category='1', difficutly='1')
-        question.insert()
-        question_id = question.id
-        res = self.client().delete(f'/questions/{question_id}')
+        res = self.client().delete('/questions/9')
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertEqual(data['deleted'], question_id)
         self.assertTrue(data['questions'])
         self.assertTrue(data['total_questions'])
 
-    'CC - test created for add_question.
+    #CC - test created for delete_question_failure.
+    def test_delete_question_failure(self):
+        res = self.client().delete('/questions/1000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Unprocessable.')
+
+    #CC - test created for add_question.
     def test_add_question(self):
-        new
-        test_question = Question(question='test question', answer='test answer', category='1', difficutly='1')
-        question.insert()
-        question_id = question.id
-        res = self.client().post('/questions/', json=test_question)
+        test_question = {'question': 'test question', 'answer': 'test answer', 'category': '1', 'difficulty': '1'}
+        res = self.client().post('/questions', json=test_question)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
-        self.assertEqual(data['created'], question_id)
-        self.assertTrue(data['questions'])
         self.assertTrue(data['total_questions'])
 
-    'CC - test created for search_questions.
+    #CC - test created for add_question_failure.
+    def test_add_question_failure(self):
+        test_question = {'question': 'test question', 'answer': 'test answer', 'category': '1', 'difficulty': '1'}
+        res = self.client().post('/questions/create', json=test_question)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Not found.')
+
+    #CC - test created for search_questions.
     def test_search_questions(self):
         test_search = {'searchTerm': 'a'}
         res = self.client().post('/questions/search', json=test_search)
@@ -93,7 +122,17 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['questions'])
         self.assertTrue(data['total_questions'])
 
-    'CC - test created for retrieve_questions_based_on_category.
+    #CC - test created for search_questions_failure.
+    def test_search_questions_failure(self):
+        test_search = {'searchTerm': 'a'}
+        res = self.client().post('/questions/', json=test_search)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Not found.')
+
+    #CC - test created for retrieve_questions_based_on_category.
     def test_retrieve_questions_based_on_category(self):
         res = self.client().get('/categories/1/questions')
         data = json.loads(res.data)
@@ -104,7 +143,16 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['total_questions'])
         self.assertTrue(data['category'])
 
-    'CC - test created for play_quiz.
+    #CC - test created for retrieve_questions_based_on_category_failure.
+    def test_retrieve_questions_based_on_category_failure(self):
+        res = self.client().get('/categories/a/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Not found.')
+
+    #CC - test created for play_quiz.
     def test_play_quiz(self):
         test_question = {'quiz_category': {'type': 'Entertainment', 'id': 5},'previous_questions': []}
         res = self.client().post('/quizzes', json=test_question)
@@ -114,7 +162,17 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], True)
         self.assertTrue(data['question'])
 
-    'CC - test created for 404 error code handler.
+    #CC - test created for play_quiz_failure.
+    def test_play_quiz_failure(self):
+        test_question = {'quiz_category': {'type': 'Entertainment', 'id': 5}}
+        res = self.client().post('/quizzes', json=test_question)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'Unprocessable.')
+
+    #CC - test created for 404 error code handler.
     def test_not_found(self):
         res = self.client().get('/categories/12345')
         data = json.loads(res.data)
@@ -122,15 +180,6 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], False)
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['message'], 'Not found.')
-
-    'CC - test created for 422 error code handler.
-    def test_unprocessable(self):
-        res = self.client().delete('/categories/12345')
-        data = json.loads(res.data)
-
-        self.assertEqual(data['success'], False)
-        self.assertEqual(res.status_code, 422)
-        self.assertEqual(data['message'], 'Unprocessable.')
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
